079f13134b3492147d592d58240976e42df9d079
ErikaD <erika.drincon@gmail.com>
2024-03-10 17:29:09 -0500

    Adding code documentation

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Importing the Scanner class to read user input
         Scanner scanner = new Scanner(System.in);
 
+        //Defining the 5 int variables to store the values
         System.out.println("Enter 5 integers: ");
         System.out.print("Number 1 = ");
         int num1 = scanner.nextInt();
@@ -17,17 +19,17 @@ public class Main {
         System.out.print("Number 5 = ");
         int num5 = scanner.nextInt();
 
+        //Defining and initializing the largest number variable
         int max = num1;
 
+        //Comparing the values and replacing the "Largest number" if the next number is greater
         max = num2 > max ? num2 : max;
         max = num3 > max ? num3 : max;
         max = num4 > max ? num4 : max;
         max = num5 > max ? num5 : max;
 
+        //To print the result of the largest value
         System.out.println("\nThe largest number is: " + max);
-
-
-
 
     }
 }
